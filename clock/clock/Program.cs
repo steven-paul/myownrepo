a540bfc9caebc1a48789eedab327d354bbb121aa
spfastjr <sfast1973@gmail.com>
2014-01-19 18:31:38 -0500

    setting alarms up

--- clock/clock/Program.cs
+++ clock/clock/Program.cs
@@ -10,10 +10,17 @@ namespace clock
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.White;
             Console.CursorVisible = false;
             Console.SetCursorPosition(0, 0);
-            
+            DateTime now= DateTime.Now;
+            List<Alarms> alarm = new List<Alarms>();
+            Console.BackgroundColor = ConsoleColor.White;
+            alarm.Add(new Alarms { hour = 8, minute = 0 , message ="Start"});
+            alarm.Add(new Alarms { hour = 10, minute = 30, message = "First break" });
+            alarm.Add(new Alarms { hour = 11, minute = 30, message = "Lunch" });
+            alarm.Add(new Alarms { hour = 12, minute = 30, message = "Back to work" });
+            alarm.Add(new Alarms { hour = 15, minute = 00, message = "Last break" });
+            alarm.Add(new Alarms { hour = 17, minute = 0, message = "Go Home" });
         }
     }
 }
