b85e08fc2e1f51628c74293f518ef2eef534cbee
spfastjr <sfast1973@gmail.com>
2014-01-19 17:54:56 -0500

    developing a console clock

---
+++ clock/clock/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clock
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
+            
+        }
+    }
+}
