1da4f893c263dfd1e63c0a77f291932d70543368
unknown <spfastjr@gmail.com>
2014-01-25 07:45:13 -0500

    working on the alarm part

---
+++ Timing/Timing/AlarmTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Timing
+{
+    public class AlarmTime
+    {
+        public int hour { get; set; }
+        public int minute { get; set; }
+        public string message { get; set; }
+    }
+}
--- Timing/Timing/Program.cs
+++ Timing/Timing/Program.cs
@@ -13,13 +13,16 @@ namespace Timing
             Console.CursorVisible = false;
             Console.ForegroundColor = ConsoleColor.White;
             Console.Clear();
-            char x = ' ';
-            while (x != 'Q' && x != 'q')
-            {
+            string exit = "";
+            while (exit != "Q")
+            {   List<AlarmTime> alarm = new List<AlarmTime>();
                 Console.SetCursorPosition(0, 0);
                 if (Console.KeyAvailable)
                 {
-                    x = Console.ReadKey(true).KeyChar;
+                    exit = InterpretKey();
+                    if (exit == "E")
+                        alarm = EditAlarmTime(alarm);
+                        
                 }
                 DateTime now = DateTime.Now;
                 WriteTime(now.Month, "/");
@@ -31,13 +34,65 @@ namespace Timing
             }
             //end of main method
         }
+
+        private static string InterpretKey()
+        {
+            return Console.ReadKey(true).KeyChar.ToString().ToUpper();
+        }
+
+        private static List<AlarmTime> EditAlarmTime(List<AlarmTime> alarm)
+        {
+            string menuChoice = "";
+            bool leaveMenu = false;
+            while (!leaveMenu)
+            {
+                if (alarm.Count == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("A - Add Alarm\nE - Exit");
+                    while (!Console.KeyAvailable)
+                    {
+
+                    }
+                    menuChoice = InterpretKey();
+                    switch (menuChoice)
+                    {
+                        case "A":
+                            alarm = AddAlarm(alarm);
+                            break;
+                        case "E":
+                            Console.Clear();
+                            leaveMenu = true;
+                            break;
+                    }
+                }
+            }
+            return alarm;
+        }
+
+        private static List<AlarmTime> AddAlarm(List<AlarmTime> alarm)
+        {
+            int hour = -1, minute = -1;
+            while (hour < 0 || hour > 23)
+                hour = NumberPrompt("Hour ");
+            while (minute < 0 || minute > 59) ;
+            minute = NumberPrompt("Minute ");
+            
+            
+            return alarm;
+        }
+
+        private static int NumberPrompt(string p)
+        {
+            throw new NotImplementedException();
+        }
         private static void WriteTime(int number, string spacer)
         {
             if (number < 10)
-            { Console.Write("0"); }
+            { 
+                Console.Write("0"); 
+            }
             Console.Write(number + spacer);
         }
-
-
     }
 }
