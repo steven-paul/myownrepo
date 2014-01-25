0e74f9a40da90d5b10a93927049e3aee2d042a30
unknown <spfastjr@gmail.com>
2014-01-24 21:52:08 -0500

    clock done.. working on alarm

--- Timing/Timing/Program.cs
+++ Timing/Timing/Program.cs
@@ -10,7 +10,29 @@ namespace Timing
     {
         static void Main(string[] args)
         {
+            char x = ' ';
+            while (x != 'Q')
+            {
+                if (Console.KeyAvailable)
+                {
+                    x = Console.ReadKey(true).KeyChar;
+                }
+                DateTime now = DateTime.Now;
+                WriteTime (now.Hour,":");
+                WriteTime(now.Minute, ":");
+                WriteTime(now.Second, "");
+                Console.WriteLine();
+                
+            }
+            //end of main method
+        }
+        private static void WriteTime(int number, string spacer)
+        {
+            if (number<10)
+            { Console.Write("0"); }
+            Console.Write(number+spacer);
+        }
 
-        }//end of main method
+      
     }
 }
